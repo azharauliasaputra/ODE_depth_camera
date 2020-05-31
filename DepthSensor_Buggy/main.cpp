57664976c567861909738622f87607628a3008a8
Azhar Aulia Saputra <azhar.aulia.s@gmail.com>
2020-05-31 19:13:52 +0900

    Initial Commit

---
+++ DepthSensor_Buggy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DepthSensor_Buggy
+//
+//  Created by Azhar Aulia Saputra on 2020/05/31.
+//  Copyright © 2020 Azhar Aulia Saputra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
